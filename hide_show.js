19c52d667b0c58255eca6d35da7a16da999772f4
Panini0805 <adrianchacon0805@gmail.com>
2024-07-19 20:15:07 -0600

    Add files via upload

---
+++ hide_show.js
@@ -0,0 +1,51 @@
+const hide_show1 = document.querySelector('#hide_show1');
+let is_clicked1 = true;
+
+let show_or_hide1 = function(){
+    if(is_clicked1){
+        hide_show1.style.display = 'block';
+        is_clicked1 = false;
+    }else{
+        hide_show1.style.display = 'none';
+        is_clicked1 = true;
+    }
+}
+
+const hide_show2 = document.querySelector('#hide_show2');
+let is_clicked2 = true;
+
+let show_or_hide2 = function(){
+    if(is_clicked2){
+        hide_show2.style.display = 'block';
+        is_clicked2 = false;
+    }else{
+        hide_show2.style.display = 'none';
+        is_clicked2 = true;
+    }
+}
+
+const hide_show3 = document.querySelector('#hide_show3');
+let is_clicked3 = true;
+
+let show_or_hide3 = function(){
+    if(is_clicked3){
+        hide_show3.style.display = 'block';
+        is_clicked3 = false;
+    }else{
+        hide_show3.style.display = 'none';
+        is_clicked3 = true;
+    }
+}
+
+const hide_show4 = document.querySelector('#hide_show4');
+let is_clicked4 = true;
+
+let show_or_hide4 = function(){
+    if(is_clicked4){
+        hide_show4.style.display = 'block';
+        is_clicked4 = false;
+    }else{
+        hide_show4.style.display = 'none';
+        is_clicked4 = true;
+    }
+}
